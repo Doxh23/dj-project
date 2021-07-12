0525749c2fec85a80aafc26743b97383c0a4f0d2
LaurentBrajkovic <laurentbrajkovic1@gmail.com>
2021-07-12 15:34:46 +0200

    added player stuff

---
+++ player.js
@@ -0,0 +1,25 @@
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+
+const playIconContainer = document.getElementById('play-icon');
+let state = 'play';
+
+const animation = lottieWeb.loadAnimation({
+    container: playIconContainer,
+    path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
+    renderer: 'svg',
+    loop: false,
+    autoplay: false,
+    name: "Demo Animation",
+});
+
+animation.goToAndStop(14, true);
+
+playIconContainer.addEventListener('click', () => {
+    if (state === 'play') {
+        animation.playSegments([14, 27], true);
+        state = 'pause';
+    } else {
+        animation.playSegments([0, 14], true);
+        state = 'play';
+    }
+});
